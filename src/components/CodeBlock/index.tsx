b33362c8e433e3ccb968f59476f4c3dd7d5707c2
xiaojun1994 <362896731@qq.com>
2022-08-22 17:39:23 +0800

    change CodeBlock style

--- src/components/CodeBlock/index.tsx
+++ src/components/CodeBlock/index.tsx
@@ -8,8 +8,8 @@ import LivePreview from '../Playground/LivePreview'
 import { copyToClipboard } from '@/utils/clipboard'
 import { NativeProps } from '@/utils/native-props'
 import { Language } from 'prism-react-renderer'
-import themeDracula from 'prism-react-renderer/themes/dracula'
-import themeMaterialLight from './themes/material-light'
+import themeOceanicNext from 'prism-react-renderer/themes/oceanicNext'
+import themeGithub from 'prism-react-renderer/themes/github'
 import classNames from 'classnames'
 import { useSafeState } from 'ahooks'
 import { HiCheck, HiOutlineClipboard } from 'react-icons/hi'
@@ -29,7 +29,7 @@ const CodeBlock: React.FC<CodeBlockProps> = props => {
   const [code, setCode] = useState(children || '')
   const [copied, setCopied] = useSafeState(false)
   const { resolvedTheme } = useTheme()
-  const theme = live ? themeDracula : resolvedTheme === 'dark' ? themeDracula : themeMaterialLight
+  const theme = live ? themeOceanicNext : resolvedTheme === 'dark' ? themeOceanicNext : themeGithub
 
   useEffect(() => {
     if (copied) {
@@ -44,12 +44,7 @@ const CodeBlock: React.FC<CodeBlockProps> = props => {
   if (!language) return <code>{children}</code>
 
   return (
-    <div
-      className={classNames('relative my-6', {
-        'border border-2 border-slate-500': live,
-        'border border-slate-200 dark:border-slate-700': !live,
-      })}
-    >
+    <div className={classNames('relative my-6 border border-slate-200 dark:border-slate-700')}>
       {/*{!live && (*/}
       {/*  <div className="absolute z-1 -top-6 right-0 flex items-center justify-between rounded-tl rounded-tr text-sm px-2 h-6 bg-slate-300 text-zinc-50 dark:bg-slate-300 dark:text-zinc-800">*/}
       {/*    <span className="font-mono">{language}</span>*/}
@@ -68,12 +63,7 @@ const CodeBlock: React.FC<CodeBlockProps> = props => {
       {/*    )}*/}
       {/*  </div>*/}
       {/*)}*/}
-      <div
-        className={classNames(style.codeBlock, {
-          [`h-[${height}]`]: height,
-          'max-h-[300px] sm:max-h-[500px]': !height,
-        })}
-      >
+      <div className={classNames(style.codeBlock, 'relative')}>
         <LiveProvider
           language={language}
           defaultCode={children?.trim()}
@@ -83,7 +73,14 @@ const CodeBlock: React.FC<CodeBlockProps> = props => {
           {/* 编辑器 */}
           {editor && (
             <div className={style.editorWrap}>
-              <div className={style.editorBody} style={{ background: theme.plain.backgroundColor }}>
+              <div
+                className={classNames(style.editorBody, {
+                  [`h-[${height}]`]: height,
+                  'max-h-[300px] sm:max-h-[500px]': !height,
+                  'border-b border-slate-200 dark:border-slate-700': live,
+                })}
+                style={{ background: theme.plain.backgroundColor }}
+              >
                 <LiveEditor className={style.editor} disabled={!live} theme={theme} />
               </div>
             </div>
