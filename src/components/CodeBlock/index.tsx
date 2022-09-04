51e043269d55e62279a4aebe968961fd38f81f7c
xiaojun1994 <362896731@qq.com>
2022-09-04 15:10:57 +0800

    Revert "change CodeBlock style"
    This reverts commit b33362c8e433e3ccb968f59476f4c3dd7d5707c2.

--- src/components/CodeBlock/index.tsx
+++ src/components/CodeBlock/index.tsx
@@ -8,8 +8,8 @@ import LivePreview from '../Playground/LivePreview'
 import { copyToClipboard } from '@/utils/clipboard'
 import { NativeProps } from '@/utils/native-props'
 import { Language } from 'prism-react-renderer'
-import themeOceanicNext from 'prism-react-renderer/themes/oceanicNext'
-import themeGithub from 'prism-react-renderer/themes/github'
+import themeDracula from 'prism-react-renderer/themes/dracula'
+import themeMaterialLight from './themes/material-light'
 import classNames from 'classnames'
 import { useSafeState } from 'ahooks'
 import { HiCheck, HiOutlineClipboard } from 'react-icons/hi'
@@ -29,7 +29,7 @@ const CodeBlock: React.FC<CodeBlockProps> = props => {
   const [code, setCode] = useState(children || '')
   const [copied, setCopied] = useSafeState(false)
   const { resolvedTheme } = useTheme()
-  const theme = live ? themeOceanicNext : resolvedTheme === 'dark' ? themeOceanicNext : themeGithub
+  const theme = live ? themeDracula : resolvedTheme === 'dark' ? themeDracula : themeMaterialLight
 
   useEffect(() => {
     if (copied) {
@@ -44,7 +44,12 @@ const CodeBlock: React.FC<CodeBlockProps> = props => {
   if (!language) return <code>{children}</code>
 
   return (
-    <div className={classNames('relative my-6 border border-slate-200 dark:border-slate-700')}>
+    <div
+      className={classNames('relative my-6', {
+        'border border-2 border-slate-500': live,
+        'border border-slate-200 dark:border-slate-700': !live,
+      })}
+    >
       {/*{!live && (*/}
       {/*  <div className="absolute z-1 -top-6 right-0 flex items-center justify-between rounded-tl rounded-tr text-sm px-2 h-6 bg-slate-300 text-zinc-50 dark:bg-slate-300 dark:text-zinc-800">*/}
       {/*    <span className="font-mono">{language}</span>*/}
@@ -63,7 +68,12 @@ const CodeBlock: React.FC<CodeBlockProps> = props => {
       {/*    )}*/}
       {/*  </div>*/}
       {/*)}*/}
-      <div className={classNames(style.codeBlock, 'relative')}>
+      <div
+        className={classNames(style.codeBlock, {
+          [`h-[${height}]`]: height,
+          'max-h-[300px] sm:max-h-[500px]': !height,
+        })}
+      >
         <LiveProvider
           language={language}
           defaultCode={children?.trim()}
@@ -73,14 +83,7 @@ const CodeBlock: React.FC<CodeBlockProps> = props => {
           {/* 编辑器 */}
           {editor && (
             <div className={style.editorWrap}>
-              <div
-                className={classNames(style.editorBody, {
-                  [`h-[${height}]`]: height,
-                  'max-h-[300px] sm:max-h-[500px]': !height,
-                  'border-b border-slate-200 dark:border-slate-700': live,
-                })}
-                style={{ background: theme.plain.backgroundColor }}
-              >
+              <div className={style.editorBody} style={{ background: theme.plain.backgroundColor }}>
                 <LiveEditor className={style.editor} disabled={!live} theme={theme} />
               </div>
             </div>
